f3ae2e66180f9a582a7bae7b05b69ef720966568
chzain3492 <zainyasin349292@gmail.com>
2025-06-17 21:54:03 -0700

    comparison

---
+++ Comparison.dart
@@ -0,0 +1,16 @@
+void main() {
+  print(10 > 5);   // true
+  print(10 == 5);  // false
+// }
+  print(10 < 5);   // false
+  print(10 >= 5);  // true
+  print(10 <= 5);  // false
+  print(10 != 5);  // true
+
+  int a = 10;
+  int b = 20;
+  
+  print(a > b);   // false
+  print(a == b);  // false
+  print(a < b);   // true
+// }
