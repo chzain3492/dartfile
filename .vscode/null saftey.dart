e3e435fd250161b1e5bb7b9532e3c1419e0190a4
chzain3492 <zainyasin349292@gmail.com>
2025-09-04 08:37:34 -0700

    null saftey

---
+++ .vscode/null saftey.dart
@@ -0,0 +1,17 @@
+void main() {
+  String name = "Zain";      // non-nullable
+  // name = null;            // ❌ Error
+
+  String? city;              // nullable
+  print(city);               // null
+
+  city = "Paris";
+  print(city?.toUpperCase());  // ✅ PARIS
+
+  String? username;
+  print(username ?? "Guest");  // ✅ Guest
+
+  username ??= "Admin";
+  print(username);             // ✅ Admin
+}
+// 
